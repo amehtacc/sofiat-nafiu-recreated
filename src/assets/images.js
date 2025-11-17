5defe8935d080262ff33a989bdaffcd1259af242
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-11-17 23:07:11 +0530

    📁 assets: added assets and created export file for easy access

---
+++ src/assets/images.js
@@ -0,0 +1,31 @@
+import Logo_Black from "../assets/logo-black.svg";
+import Logo_Yellow from "../assets/logo-yellow.svg";
+import Arrow_Down from "../assets/arrow-down.svg";
+import Arrow_Long_Yellow from "../assets/arrow-long-yellow.svg";
+import Arrow_Small_Yellow from "../assets/arrow-small-yellow.svg";
+import Arrow from "../assets/arrow.svg";
+import Ecoremind_Overlay_1 from "../assets/ecoremind-overlay-1.svg";
+import Ecoremind_Overlay_2 from "../assets/ecoremind-overlay-2.svg";
+import Edueat_Overlay_1 from "../assets/edueat-overlay-1.svg";
+import Edueat_Overlay_2 from "../assets/edueat-overlay-2.svg";
+import Glasgopal_Overlay_1 from "../assets/glasgopal-overlay-1.svg";
+import Glasgopal_Overlay_2 from "../assets/glasgopal-overlay-2.svg";
+import Spintowin_Overlay_1 from "../assets/spintowin-overlay-1.svg";
+import Spintowin_Overlay_2 from "../assets/spintowin-overlay-2.svg";
+
+export {
+  Logo_Black,
+  Logo_Yellow,
+  Arrow_Down,
+  Arrow_Long_Yellow,
+  Arrow_Small_Yellow,
+  Arrow,
+  Ecoremind_Overlay_1,
+  Ecoremind_Overlay_2,
+  Edueat_Overlay_1,
+  Edueat_Overlay_2,
+  Glasgopal_Overlay_1,
+  Glasgopal_Overlay_2,
+  Spintowin_Overlay_1,
+  Spintowin_Overlay_2,
+};
