39f9c41fff6137420c15a16e25d22b6b6fc8cdc9
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-11-23 00:58:08 +0530

    ✨ feat: added the scroll container with scroll transition

--- src/components/ScrollContainer.jsx
+++ src/components/ScrollContainer.jsx
@@ -1,30 +1,62 @@
 import React, { useRef } from "react";
 import { motion, useScroll, useTransform } from "motion/react";
-import {Arrow_Small_Yellow} from "../assets/images"
+import { Arrow_Small_Yellow } from "../assets/images";
 
 function ScrollContainer() {
-  const scrollXRef = useRef(null);
+  const refScrollX = useRef(null);
   const { scrollYProgress } = useScroll({
-    target: scrollXRef,
-    offset: ["start end", "end start"],
+    target: null,
+    offset: ["start", "end"],
   });
-  const scrollX = useTransform(scrollYProgress, [0, 1], ["20px, 20px"])
+  const scrollX = useTransform(scrollYProgress, [0, 1], ["0px", "-85px"]);
   return (
-    <div ref={scrollXRef} className="relative w-full h-[180px] overflow-hidden bg-black">
-      <div className="flex items-center">
-        <span className="flex pr-[100px]">
-            {"Selected Works "}
-            <img className="w-20 ml-10" src={Arrow_Small_Yellow} alt="Arrow_Small_Yellow" />
+    <div className="relative w-full h-[180px] overflow-hidden bg-black">
+      <motion.div
+        ref={refScrollX}
+        style={{ translateX: scrollX }}
+        className="flex"
+      >
+        <span className="w-fit flex pr-10 text-white font-gabarito text-[112px] shrink-0 items-center">
+          {"Selected Works "}
+          <img
+            className="w-20 ml-10"
+            src={Arrow_Small_Yellow}
+            alt="Arrow_Small_Yellow"
+          />
         </span>
-        <span className="flex pr-[100px]">
-            {"Selected Works "}
-            <img className="w-20 ml-10" src={Arrow_Small_Yellow} alt="Arrow_Small_Yellow" />
+        <span className="w-fit flex pr-10 text-white font-gabarito text-[112px] shrink-0 items-center">
+          {"Selected Works "}
+          <img
+            className="w-20 ml-10"
+            src={Arrow_Small_Yellow}
+            alt="Arrow_Small_Yellow"
+          />
         </span>
-        <span className="flex pr-[100px]">
-            {"Selected Works "}
-            <img className="w-20 ml-10" src={Arrow_Small_Yellow} alt="Arrow_Small_Yellow" />
+        <span className="w-fit flex pr-10 text-white font-gabarito text-[112px] shrink-0 items-center">
+          {"Selected Works "}
+          <img
+            className="w-20 ml-10"
+            src={Arrow_Small_Yellow}
+            alt="Arrow_Small_Yellow"
+          />
         </span>
-      </div>
+        <span className="w-fit flex pr-10 text-white font-gabarito text-[112px] shrink-0 items-center">
+          {"Selected Works "}
+          <img
+            className="w-20 ml-10"
+            src={Arrow_Small_Yellow}
+            alt="Arrow_Small_Yellow"
+          />
+        </span>
+        <span className="w-fit flex pr-10 text-white font-gabarito text-[112px] shrink-0 items-center">
+          {"Selected Works "}
+          <img
+            className="w-20 ml-10"
+            src={Arrow_Small_Yellow}
+            alt="Arrow_Small_Yellow"
+          />
+        </span>
+      </motion.div>
     </div>
   );
 }
