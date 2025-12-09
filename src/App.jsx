9757d730bcb7cbacc95cae57f1be20cb5ec8f9b8
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-12-09 11:11:05 +0530

    🧩 feat: add how-to-process and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Header from './components/Header'
-import Hero from './sections/Hero'
-import Works from './sections/Works'
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./sections/Hero";
+import Works from "./sections/Works";
+import HowToProcess from "./sections/HowToProcess";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
-    <div className='relative max-w-[1550px] mx-auto'>
+    <div className="relative max-w-[1550px] mx-auto">
       <Header />
       <main>
         <Hero />
         <Works />
+        <HowToProcess />
       </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
